27dcf54830834c39c313051a3564eb9a779ec777
Philipp Melnikov <fmelnikov123@gmail.com>
2017-07-04 12:58:45 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -10,30 +10,30 @@ $(document).ready(function() {
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
-		effect: 'fade'
+    effect: 'fade'
   }).disableTouchControl();
 
   // margin for caption
   function setBannerCaptionMargin() {
-    var capMargin = ($(document).width() - $(".h-contacts").width())/2;
+    var capMargin = ($(document).width() - $(".h-contacts").width()) / 2;
     $(".banner-caption").css("margin-left", capMargin);
     $(".top-slider-pagination").css("left", capMargin);
   }
   setBannerCaptionMargin();
-  $(window).resize(function () {
+  $(window).resize(function() {
     setBannerCaptionMargin();
   });
 
   var galleryTopConfig = {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        noSwiping: true,
-        loop: true
-    };
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    noSwiping: true,
+    loop: true
+  };
   var galleryTop = new Swiper('#gallery-top-1', galleryTopConfig).disableTouchControl();
 
-  $('.plant-is-switcher').click(function(){
-    var activePlantIsContent = "#" + "data-index" +  $(this).data("index");
+  $('.plant-is-switcher').click(function() {
+    var activePlantIsContent = "#" + "data-index" + $(this).data("index");
 
     $('.plant-is-content').removeClass('active');
     $(activePlantIsContent).addClass('active');
@@ -45,31 +45,32 @@ $(document).ready(function() {
 
   });
 
-    var reviewSwiper = new Swiper('.reviews-slider', {
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-      spaceBetween: 0,
-      autoplay: 0,
-      autoplayDisableOnInteraction: false,
-      slidesPerView: 1,
-      centeredSlides: true,
-      loop: true
-    }).disableTouchControl();
+  var reviewSwiper = new Swiper('.reviews-slider', {
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 0,
+    autoplay: 0,
+    autoplayDisableOnInteraction: false,
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true
+  }).disableTouchControl();
 
-    // up button
+  // up button
 
-    $(this).scroll(function() {
-      var fadeOutPos = $('.main-title').offset().top;
-      if($(this).scrollTop() >= fadeOutPos){
-        $('.up-button').fadeIn(300);
-      }
-      else{
-          $('.up-button').fadeOut(300);
-      }
-    })
-    $('.up-button').click(function () {
-      $("html, body").animate({ scrollTop: 0 }, "slow");
-    })
+  $(this).scroll(function() {
+    var fadeOutPos = $('.main-title').offset().top;
+    if ($(this).scrollTop() >= fadeOutPos) {
+      $('.up-button').fadeIn(300);
+    } else {
+      $('.up-button').fadeOut(300);
+    }
+  })
+  $('.up-button').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+  })
 
 })
 
@@ -82,7 +83,7 @@ $(document).ready(function() {
     columns.each(function(i, el) {
         currentHeight = $(el).height();
 
-        if(currentHeight > tallestcolumn) {
+        if (currentHeight > tallestcolumn) {
           tallestcolumn = currentHeight;
         }
       }
@@ -94,96 +95,146 @@ $(document).ready(function() {
 
   //Выравнивание по высоте блоков в braces-types
 
-//выпадающее меню info-menu-drop
-   $('.menu-link').on('click', function (e) {
+  //выпадающее меню info-menu-drop
+  $('.menu-link').on('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
-    var  $this = $(this),
-         item = $(this).closest(".menu-item"),
-         list = $(".menu-list"),
-         links = $(".menu-link"),
-         content = item.find(".menu-list-in"),
-         otherContent = list.find(".menu-list-in"),
-         duration = 100;
+    var $this = $(this),
+      item = $(this).closest(".menu-item"),
+      list = $(".menu-list"),
+      links = $(".menu-link"),
+      content = item.find(".menu-list-in"),
+      otherContent = list.find(".menu-list-in"),
+      duration = 100;
 
-    if   (!$this.hasClass('active')) {
-         links.removeClass('active');
-         $this.addClass('active');
+    if (!$this.hasClass('active')) {
+      links.removeClass('active');
+      $this.addClass('active');
 
       otherContent.stop(true, true).slideUp(duration);
       content.stop(true, true).slideDown(duration);
       setEqualHeight(content.find("li[class*='col']"));
     } else {
-      $this.delay(duration).queue(function () {
-         $(this).removeClass('active');
-         $(this).dequeue();
-       });
+      $this.delay(duration).queue(function() {
+        $(this).removeClass('active');
+        $(this).dequeue();
+      });
 
       content.stop(true, true).slideUp(duration);
       setEqualHeight(content.find("li[class*='col']"));
     }
     var currentMenu = $this;
     $(window).click(function() {
-      setTimeout(function () {
-         $(currentMenu).removeClass('active');
-       }, 100);
+      setTimeout(function() {
+        $(currentMenu).removeClass('active');
+      }, 100);
       content.slideUp(100);
+    });
   });
-  });
-  $('.menu-list-in').on('click', function (e){
+
+  $('.menu-list-in').on('click', function(e) {
     e.stopPropagation();
   });
-  //offices-map
-    $('.btn-offices-link').click(function (e) {
+
+  // Нижние выпадающие меню начало
+  $('.main-menu-link.link-arrow').hover(function(e) {
     e.preventDefault();
     e.stopPropagation();
-    var  $this = $(this),
-         item = $this.closest(".btn-offices-item"),
-         list = $(".btn-offices-list"),
-         links = list.find(".btn-offices-link");
+    var $this = $(this),
+      item = $(this).closest(".menu-item"),
+      list = $(".menu-list"),
+      links = $(".menu-link"),
+      content = item.find(".menu-list-in"),
+      otherContent = list.find(".menu-list-in"),
+      duration = 100;
 
-    if   (!$this.hasClass('active')) {
+    if (!$this.hasClass('active')) {
+      links.removeClass('active');
+      $this.addClass('active');
 
-         links.removeClass('active');
-         $this.addClass('active');
+      otherContent.stop(true, true).slideUp(duration);
+      content.stop(true, true).slideDown(duration);
+      setEqualHeight(content.find("li[class*='col']"));
     }
 
-    var  address = $(".offices-location"),
-         itemPosition = $this.data('results');
-
-         address.filter('.results-item-'+itemPosition)
-         .addClass('active')
-         .siblings()
-         .removeClass('active');
+  }, function(e) {
+    var item = $(this).closest(".menu-item"),
+      list = $(".menu-list"),
+      links = $(".menu-link"),
+      content = item.find(".menu-list-in"),
+      $this = $(this);
+    if (!$(content).is(":hover")) {
+      setTimeout(function() {
+        $($this).removeClass('active');
+      }, 100);
+      content.slideUp(100);
+    }
   });
 
-  var   infoBuyItemH6 = $(".info-buy-item>h6"),
-        officeAddress = $(".list-office-address"),
-        officePhone = $(".office-phone-list");
+  $('.menu-list-in').hover(function(e) {
+    e.stopPropagation();
+  }, function() {
+    var mainMenuLink = $(this).siblings(".menu-link");
+    if (!$(mainMenuLink).is(":hover")) {
+      setTimeout(function() {
+        $(mainMenuLink).removeClass('active');
+      }, 100);
+      $(this).slideUp(100);
+    }
+  })
+  // Нижние выпадающие меню конец
+  
+  //offices-map
+  $('.btn-offices-link').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var $this = $(this),
+      item = $this.closest(".btn-offices-item"),
+      list = $(".btn-offices-list"),
+      links = list.find(".btn-offices-link");
 
-    // setEqualHeight(infoBuyItemH6);
-    // setEqualHeight(officeAddress);
-    // setEqualHeight(officePhone);
+    if (!$this.hasClass('active')) {
+
+      links.removeClass('active');
+      $this.addClass('active');
+    }
+
+    var address = $(".offices-location"),
+      itemPosition = $this.data('results');
+
+    address.filter('.results-item-' + itemPosition)
+      .addClass('active')
+      .siblings()
+      .removeClass('active');
+  });
+
+  var infoBuyItemH6 = $(".info-buy-item>h6"),
+    officeAddress = $(".list-office-address"),
+    officePhone = $(".office-phone-list");
+
+  // setEqualHeight(infoBuyItemH6);
+  // setEqualHeight(officeAddress);
+  // setEqualHeight(officePhone);
 });
 
 
 
- $(document).mouseup(function (e){
+$(document).mouseup(function(e) {
 
-       var  item = $(".menu-item"),
-            list = $(".menu-list-in"),
-            link = $(".menu-link"),
-            duration = 100;
+  var item = $(".menu-item"),
+    list = $(".menu-list-in"),
+    link = $(".menu-link"),
+    duration = 100;
 
-      if (!list.is(e.target) &&
-          !link.is(e.target) &&
-           list.has(e.target).length === 0) {
+  if (!list.is(e.target) &&
+    !link.is(e.target) &&
+    list.has(e.target).length === 0) {
 
-         link.delay(duration).queue(function () {
-           $(this).removeClass('active');
-           $(this).dequeue();
-         });
+    link.delay(duration).queue(function() {
+      $(this).removeClass('active');
+      $(this).dequeue();
+    });
 
-         list.slideUp(duration);
-      }
-   });
+    list.slideUp(duration);
+  }
+});
